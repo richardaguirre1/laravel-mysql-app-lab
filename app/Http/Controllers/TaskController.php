a3ec711fa923c27acae485fccad8f8bd0c6593a3
Richard Aguirre <richardaguirreaz@hotmail.com>
2025-01-05 12:59:17 -0300

    Codigo Laravel

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+use Illuminate\View\View;
+
+use Illuminate\Support\Facades\Redirect;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $tasks = Task::latest()->paginate(10);
+        return view('index', ['tasks' => $tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse 
+    {
+
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+
+        ]);
+
+        Task::create($request->all());
+        return redirect()->route('tasks.index')->with('success', 'Nueva tarea Creada');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task): View
+    {
+        return view('edit', ['task' => $task]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task): RedirectResponse
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+
+        ]);
+        $task->update($request->all());
+        return redirect()->route('tasks.index')->with('success', 'Tarea Actualizada');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('tasks.index')->with('success', 'Tarea Eliminada');
+    }
+}
